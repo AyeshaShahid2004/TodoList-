a0149efc33ad3b55b423b539064efd8364611b0e
AyeshaShahid2004 <ayesha20040831@gmail.com>
2025-07-12 21:49:29 +0500

    My first React project

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,6 +5,7 @@ export default function TodoList(props) {
    const {todos} = props
   return (
     <ul className='main'>
+      {/* hy */}
         {todos.map((todo, todoIndex)=> {
             return (
                 <ToCard{...props} key={todoIndex} index=
