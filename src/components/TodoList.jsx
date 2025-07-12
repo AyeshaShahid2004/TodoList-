15c7567114c6c64edf6035ec050784781a22b1ae
abdullahshahid <abdullahshahid5112@gmail.com>
2025-07-12 21:27:45 +0500

    Todo list

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ToCard from './ToCard'
+
+export default function TodoList(props) {
+   const {todos} = props
+  return (
+    <ul className='main'>
+        {todos.map((todo, todoIndex)=> {
+            return (
+                <ToCard{...props} key={todoIndex} index=
+                {todoIndex}> 
+                <p>{todo}</p>
+                </ToCard>
+            )
+        })}
+    </ul>
+  )
+}
